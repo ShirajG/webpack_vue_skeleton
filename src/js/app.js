3850076e55d19cab8fc4021766e99b8d92be9a72
Shiraj Ganguly <shirajganguly@gmail.com>
2016-11-25 17:58:11 -0500

    Add bootstrap and jquery to the bundle

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,3 @@
+require('bootstrap-loader');
+require('../scss/app.scss');
 require('../../public/css/app.css');
--- webpack.config.js
+++ webpack.config.js
@@ -7,9 +7,30 @@ module.exports = {
     path: path.join('public', 'js'),
     filename: "bundle.js"
   },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery"
+    })
+  ],
   module: {
     loaders: [
-      { test: /\.css$/, loader: "style!css"}
+      {
+        test: /\.css$/,
+        loaders: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.scss$/,
+        loaders: ["style-loader", "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.svg$/,
+        loader: "url-loader"
+      },
+      {
+        test: /\.(woff2?|ttf|eot|svg)$/,
+        loader: 'url-loader'
+      }
     ]
   }
 }
